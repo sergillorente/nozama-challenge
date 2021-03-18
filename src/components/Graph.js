7d4386ffcc28ebf788a704c110a118049121d8aa
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-03-18 22:56:56 +0100

    data.json file updated with x and y, further props added to Graph.js and some props coming from Dashboard.js to make the Graph component reusable

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -7,43 +7,50 @@ import data from './../lib/data.json'
 // is very whitish and to the top is gets greenish
 defaults.global.tooltips.enabled = false
 
-const Graph = () => {
-    const fetchData = data.map((dataObj, index) => {
-        return dataObj.Recovered
-    })
-    return(
-        <div>
-           <Line
-               data={{
-                labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-                datasets: [
+
+const Graph = ({ labels, data, units }) => {
+
+    const chartData = (canvas) => {
+        const ctx = canvas.getContext("2d")
+        const gradient = ctx.createLinearGradient(0, 0, 0, 250)
+        gradient.addColorStop(0, 'rgba(100,124,100,1)')
+        gradient.addColorStop(1, 'rgba(100,124,100,0)')
+
+        return {
+            labels,
+            datasets: [
+                {
+                    data,
+                    label: 'Recovered',
+                    borderColor: 'rgba(100,124,100,1)',
+                    borderWidth: 1,
+                    pointRadius: 7,
+                    pointBackgroundColor: 'white',
+                    backgroundColor: gradient
+                }
+            ]
+        }
+    }
+
+    return <Line
+        data={chartData}
+        options={{
+            // maintainAspectRatio: false,
+            scales: {
+                yAxes: [
                     {
-                        label: '# of votes',
-                        data: [12, 19, 3, 5, 2, 3], // fetchData
-                        borderColor: 'rgba(73, 126, 48)'
+                        ticks: {
+                            beginAtZero: true,
+                            callback: function (value) { return `${value}${units}` }
+                        }
                     }
                 ]
-               }} 
-               height={400}
-               width={600}
-               options={{
-                   maintainAspectRatio: false, 
-                   scales: {
-                     yAxes: [
-                         {
-                            ticks: {
-                                beginAtZero: true
-                            }
-                         }
-                     ]  
-                   },
-                   legend: {
-                       display: false
-                   }
-               }}
-           /> 
-        </div>
-    )
+            },
+            legend: {
+                display: false
+            }
+        }}
+    />
 }
 
 export default Graph
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from 'react'
 
+import data from '../lib/data.json'
+
 import './Dashboard.css'
 import Graph from './../components/Graph'
 
 const Dashboard = () => {
-
-    const [ selectedDate, setSelectedDate ] = useState('')
-    const [ TRR, setTRR ] = useState('')
-    const [ recovered, setRecovered ] = useState('')
-    const [ graphData, setGraphData ] = useState('')
+    const [selectedDate, setSelectedDate] = useState('')
+    const [TRR, setTRR] = useState('')
+    const [recovered, setRecovered] = useState('')
 
     useEffect(() => {
-       // transform data for graph[] 
+        // transform data for graph[] 
     })
 
     useEffect(() => {
         // update values [date] 
-     })
+    })
 
     return (
         <div>
             <img className="dashboard__logo" src="/images/nozama_logo.png" alt="logo" />
             <h1 className="dashboard__title">Challenge</h1>
-            <Graph />
+            <Graph
+                data={data}
+                labels={data.map(value => value.x)}
+                units='kg.'
+            />
         </div>
     )
 }
