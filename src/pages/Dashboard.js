660e1e733f262e37c13366a5dd27470017035ce5
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-03-18 11:40:33 +0100

    Hooks added as well as the graph data sample

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Dashboard.css'
 
 const Dashboard = () => {
+
+    const [ date, setDate ] = useState('')
+    const [ TRR, setTRR ] = useState('')
+    const [ recovered, setRecovered ] = useState('')
+    const [ graphData, setGraphData ] = useState('')
+
+    useEffect(() => {
+       // transform data for graph[] 
+    })
+
+    useEffect(() => {
+        // update values [date] 
+     })
+
     return (
         <div>
             <img className="dashboard__logo" src="/images/nozama_logo.png" alt="logo" />
